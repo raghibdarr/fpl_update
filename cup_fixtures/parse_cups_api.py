48f412c53e3a300d49746b9f22d7c34b3d7b078b
raghibdarr <raghibdarr@gmail.com>
2024-09-12 22:46:14 +0100

    Get IDs of all premier league teams from the API

--- cup_fixtures/parse_cups_api.py
+++ cup_fixtures/parse_cups_api.py
@@ -1,8 +1,10 @@
 import http.client
 import os
 from dotenv import load_dotenv
+import json
 
 # Competition IDs for various cups already retrieved previously via API
+PREMIER_LEAGUE_ID = 1
 FA_CUP_ID = 21
 EFL_CUP_ID = 22
 UCL_ID = 24
@@ -24,9 +26,27 @@ headers = {
     'x-rapidapi-host': "football-web-pages1.p.rapidapi.com"
 }
 
-# Make a GET request to the API endpoint
-# This specific request is for fixtures-results of competition with ID 24 (UCL)
+### Make GET requests to the API endpoint ##
+
+# Get premier league teams
+conn.request("GET", "/teams.json?comp=" + str(PREMIER_LEAGUE_ID), headers=headers)
+prem_teams = conn.getresponse()
+prem_teams_data = prem_teams.read()
+
+# Parse the JSON data
+prem_teams_json = json.loads(prem_teams_data.decode("utf-8"))
+
+# Extract only the IDs into a list
+prem_team_ids = [team['id'] for team in prem_teams_json['teams']]
+
+# Print the list of Premier League team IDs
+print("Premier League Team IDs:", prem_team_ids)
+
+
+# Get the fixtures/results of UCL
 conn.request("GET", "/fixtures-results.json?comp=24", headers=headers)
+
+# TO-DO: Filter the data to only include fixtures/results of premier league teams
 
 # Get the response from the API
 res = conn.getresponse()
@@ -34,4 +54,4 @@ res = conn.getresponse()
 data = res.read()
 
 # Print the decoded response data
-print(data.decode("utf-8"))
+#print(data.decode("utf-8"))
